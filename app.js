a3b43bd40abf7cebd142973dab960f634f4b5897
Mukul Srivastav <mukulsrivastav551@gmail.com>
2023-04-26 12:51:45 +0530

    changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const router = require('./routes/router');
 app.use(express.json());
 app.use(cors());
 app.use(router);
-const port = 8005;
+const port =process.env.PORT || 8005;
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
